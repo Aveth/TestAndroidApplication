247214633e86d184245bac83ab70d0eecdd02fcc
avais <asethi@pelmorex.com>
2019-06-20 09:21:10 -0400

    update

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -13,9 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        cardRecyclerView.adapter = adapter
-        cardRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 }
